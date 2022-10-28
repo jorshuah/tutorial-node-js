59675a0377f72f84e3b32b3e4b5778c7d25dc779
Joshua Ademola <networkcoders@gmail.com>
2022-10-28 13:32:14 +0100

    First Commit

---
+++ modules.js
@@ -0,0 +1,10 @@
+// Modules
+const {john, peter} = require('./names');
+const sayHi = require("./utils");
+const data = require("./alternative-flavor")
+require("./mind-grenade")
+
+
+sayHi('Susan')
+sayHi(john)
+sayHi(peter)
---
+++ names.js
@@ -0,0 +1,9 @@
+
+// local
+const secret = 'SUPER SECRET'
+
+// Share
+const john = 'John'
+const peter = 'Peter'
+
+module.exports = {john, peter}
